446916f02557273d86787323b65b05eee2a56a6b
zarir94 <shahriar@gmail.com>
2024-11-06 02:09:28 -0800

    add sandbox

--- src/routes/watch-[type]-[id]/+page.svelte
+++ src/routes/watch-[type]-[id]/+page.svelte
@@ -7,7 +7,7 @@
 <form>
 <div class="box">
   <h3 class="title is-size-3">{data.title}</h3>
-  <iframe class="main-frame" src="{data.vidURL}" title="{data.title}" frameborder="0"></iframe>
+  <iframe class="main-frame" src="{data.vidURL}" title="{data.title}" frameborder="0"  sandbox="allow-scripts allow-same-origin"></iframe>
   <div class="is-flex is-justify-content-center is-gap-2">
     {#each Object.entries(data.servers) as [name, i]}
       <button class="button is-medium {data.currentServer == name ? 'is-primary' : 'is-info'}" type="submit" name="server" value="{parseInt(i) + 1}">{name}</button>
