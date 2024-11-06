2b6c2864be907f87b72383d7bbe6291243fb871e
zarir94 <shahriar@gmail.com>
2024-11-05 23:57:27 -0800

    up

--- src/lib/cached.js
+++ src/lib/cached.js
@@ -1,10 +1,12 @@
 import { getLatestItems } from '$lib/tmdb_api.js';
 import * as fs from "fs";
 
+const suffix = import.meta.env.PROD ? '/' : '';
+
 function writeJSON(name, obj) {
   return new Promise((resolve, reject)=>{
     try {  
-      fs.writeFile(name, JSON.stringify(obj), ()=>{resolve()});
+      fs.writeFile(suffix + name, JSON.stringify(obj), ()=>{resolve()});
     } catch (err) {reject(err)}
   })
 }
@@ -12,7 +14,7 @@ function writeJSON(name, obj) {
 function readJSON(name) {
   return new Promise((resolve, reject)=>{
     try {
-      fs.readFile(name, (err, data)=>{
+      fs.readFile(suffix + name, (err, data)=>{
         if (err) reject(err);
         resolve(JSON.parse(data));
       })
@@ -22,22 +24,22 @@ function readJSON(name) {
 
 const data = {
 	getMovies: async function (update = false) {
-    if (!fs.existsSync('/tmp/cachedMovies.json') || update) {
+    if (!fs.existsSync('tmp/cachedMovies.json') || update) {
       console.log(`Fetching Data ${Math.round(Math.random() * 100000)}...`);
       let cachedMovies = await Promise.all([ getLatestItems('movie', 1), getLatestItems('movie', 2), getLatestItems('movie', 3) ]);
       cachedMovies = cachedMovies.flat();
-      await writeJSON('/tmp/cachedMovies.json', cachedMovies);
+      await writeJSON('tmp/cachedMovies.json', cachedMovies);
     }
-    return await readJSON('/tmp/cachedMovies.json');
+    return await readJSON('tmp/cachedMovies.json');
   },
   getTVs: async function (update = false) {
-    if (!fs.existsSync('/tmp/cachedTVs.json') || update) {
+    if (!fs.existsSync('tmp/cachedTVs.json') || update) {
       console.log(`Fetching Data ${Math.round(Math.random() * 100000)}...`);
       let cachedTVs = await Promise.all([ getLatestItems('tv', 1), getLatestItems('tv', 2), getLatestItems('tv', 3) ]);
       cachedTVs = cachedTVs.flat();
-      await writeJSON('/tmp/cachedTVs.json', cachedTVs);
+      await writeJSON('tmp/cachedTVs.json', cachedTVs);
     }
-    return await readJSON('/tmp/cachedTVs.json');
+    return await readJSON('tmp/cachedTVs.json');
   },
 };
 
