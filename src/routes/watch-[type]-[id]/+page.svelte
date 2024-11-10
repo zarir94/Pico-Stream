37dacbca4f4c7f8b8f6659b75d92654be3ad2a98
zarir94 <shahriar@gmail.com>
2024-11-10 02:03:53 -0800

    fix one

--- src/routes/watch-[type]-[id]/+page.svelte
+++ src/routes/watch-[type]-[id]/+page.svelte
@@ -4,11 +4,13 @@
   import { beforeNavigate, afterNavigate, goto } from "$app/navigation";
 
   let disabled = false;
+  let showisk = false;
   beforeNavigate(()=>{
     disabled = true;
   })
   afterNavigate(()=>{
     disabled = false;
+    showisk = false;
   })
 
   function submitForm() {
@@ -16,6 +18,7 @@
     if (this.tagName == 'BUTTON') {
       url.searchParams.set(this.name, this.value)
     }
+    showisk = true;
     goto(url, {replaceState: true, noScroll: true, keepFocus: true});
   }
 </script>
@@ -26,7 +29,11 @@
 <div class="block has-background-black-ter has-shadow p-5 is-rounded">
   <h3 class="title is-size-3">{data.title}</h3>
   <!-- sandbox="allow-scripts allow-same-origin" -->
-  <iframe class="main-frame is-rounded" src="{data.vidURL}" title="{data.title}" frameborder="0"></iframe>
+  {#if showisk}
+    <div class="skeleton-block main-frame is-rounded" style="margin-bottom: 0px;"></div>
+  {:else}
+    <iframe class="main-frame is-rounded skeleton-block" style="margin-bottom: 0px;" on:load={function () {this.classList.remove('skeleton-block')}} src="{data.vidURL}" title="{data.title}" frameborder="0"></iframe>
+  {/if}
   <h4 class="has-text-centered title is-4 my-4">Servers</h4>
   <div class="is-flex is-justify-content-center is-flex-wrap-wrap is-gap-1 my-4">
     {#each Object.entries(data.servers) as [name, i]}
@@ -43,7 +50,7 @@
     <h4 class="has-text-centered title is-4 my-4">Episodes</h4>
     <div class="grid is-col-min-8">
       {#each data.episodes as e}
-      <button on:click={submitForm} class="button is-dark is-normal {data.currentEpisode == e.no ? 'is-primary' : 'is-dark'}" {disabled} type="submit" name="e" value="{e.no}">{e.no}. {e.name} {e.rating}</button>
+      <button on:click={submitForm} class="epi button is-dark is-normal {data.currentEpisode == e.no ? 'is-primary' : 'is-dark'}" {disabled} type="submit" name="e" value="{e.no}"><span>{e.no}. {e.name}</span></button>
       {/each}
     </div>
   {/if}
@@ -59,5 +66,10 @@
   }
   button {
     border-radius: 7px;
+  }
+  button.epi span {
+    overflow: hidden; 
+    text-overflow: ellipsis;
+    white-space: nowrap;
   }
 </style>
