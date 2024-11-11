66cedc8d5eeaee324f6b77c0a1ef331b98ef2c19
zarir94 <shahriar@gmail.com>
2024-11-11 03:54:06 -0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
+import purge from '@erbelion/vite-plugin-sveltekit-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
+		purge({ paths: ['src/**/*.{svelte,html}', 'src/lib/design/*.{svelte,html}'] })
 	]
 });
 
