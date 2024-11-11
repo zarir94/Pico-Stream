02efa84e98857f651a2b0174abed9a96df62004e
zarir94 <shahriar@gmail.com>
2024-11-11 03:03:11 -0800

    remove purge old

--- svelte.config.js
+++ svelte.config.js
@@ -3,16 +3,8 @@ import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	transformers: {
-		scss: {
-			includePaths: ['node_modules', 'src']
-		}
-	},
 	kit: {
 		adapter: adapter(),
-		version: {
-			name: +new Date() + ''
-		}
 	},
 	preprocess: sveltePreprocess()
 };
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-import purge from '@erbelion/vite-plugin-sveltekit-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
-		purge({ paths: ['src/**/*.{svelte,html}', 'src/lib/design/*.{svelte,html}'] })
 	]
 });
 
