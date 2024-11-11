d36f08547c0db8d433e77fcd96f6b9800ccd81a8
zarir94 <shahriar@gmail.com>
2024-11-11 01:25:16 -0800

    change version

--- vite.config.js
+++ vite.config.js
@@ -6,15 +6,6 @@ export default defineConfig({
 	plugins: [
 		sveltekit(),
 		purge({ paths: ['src/**/*.{svelte,html}', 'src/lib/design/*.{svelte,html}'] })
-	],
-	build: {
-		rollupOptions: {
-			output: {
-				entryFileNames: 'assets2/[name].edited[hash].js',
-				chunkFileNames: 'assets2/[name].edited[hash].js',
-				assetFileNames: 'assets2/[name].edited[hash].[ext]'
-			}
-		}
-	}
+	]
 });
 
