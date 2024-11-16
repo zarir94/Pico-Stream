426b7297d370cfd33964a082b600dbb7a7a12cea
zarir94 <shahriar@gmail.com>
2024-11-16 03:13:34 -0800

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,9 @@
-import adapter from '@sveltejs/adapter-node';
 import { sveltePreprocess } from 'svelte-preprocess';
+import adapterNode from '@sveltejs/adapter-node';
+import adapterVercel from '@sveltejs/adapter-vercel';
+
+const adapter = process.env.ADAPTER === 'vercel' ? adapterVercel : adapterNode;
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
